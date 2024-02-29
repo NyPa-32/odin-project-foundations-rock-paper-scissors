f4485c7b5917e2d6b61b03b690ad38b546c431de
NyPa-32 <thny.coding@gmail.com>
2024-02-29 08:06:29 +0100

    Add "playRound" function Add const playerSelection Add const computerSelection

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+
+
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * 3);
 
     return choices[random];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
 }
 
 console.log(getComputerChoice())
