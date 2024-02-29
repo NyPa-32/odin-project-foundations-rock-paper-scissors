665b62967843d6b737135d2a797971196407383a
NyPa-32 <thny.coding@gmail.com>
2024-02-29 09:10:00 +0100

    Add and finish playRound function Working game logic

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+let Choices = ["Rock", "Paper", "Scissors"];
+
 const computerSelection = getComputerChoice();
-const playerSelection = "Rock";
+const playerSelection = "Scissors";
 
 
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    let random = Math.floor(Math.random() * 3);
+    let random = Math.floor(Math.random() * Choices.length);
 
-    return choices[random];
+    return Choices[random];
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    }
+    else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+        return ("Computer Wins!");
+    }
+    else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
+        return ("You Win!");
+    }
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+        return ("You Win!");
+    }
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
+        return ("Computer Wins!");
+    }
+    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+        return ("Computer Wins!");
+    }
+    else {
+        return ("You Win!");
+    }
 }
 
-console.log(getComputerChoice())
+console.log(playRound(playerSelection, computerSelection))
+
+//(playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") 
